2f5a063c19f5ba616bf5b53b1df7901faf23ce95
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-01-28 11:30:44 +0700

    Add Button component and good button

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,9 @@
+import { set } from "mongoose";
 import { useState } from "react";
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,10 +11,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleGood = () => {
+    // console.log("dididi");
+    setGood(good + 1);
+  };
+
   return (
     <>
       <h1>give feedback</h1>
       {/* buttons */}
+      <Button onClick={handleGood} text="good" />
       <h1>statistics</h1>
       {/* good, neutral, bad */}
     </>
