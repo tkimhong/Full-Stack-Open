55cfe0b90e70f30694dbe45c06ba66c5a43e9db0
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-02-01 20:00:54 +0700

    Map parts array in Content

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -2,9 +2,9 @@ const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
   </div>
 );
 
