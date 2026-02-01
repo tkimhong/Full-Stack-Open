481c3c87a60922757c774556207a2902f8a79395
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-02-01 20:28:02 +0700

    Extract Course component to a module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,38 +1,4 @@
-const Header = (props) => <h2>{props.course}</h2>;
-
-const Content = (props) => (
-  <div>
-    {props.parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-);
-
-const Total = (props) => (
-  <p>
-    <strong>total of {props.total} exercises</strong>
-  </p>
-);
-
-const Course = (props) => {
-  const total = props.course.parts.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue.exercises;
-  }, 0);
-
-  return (
-    <>
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total total={total} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
