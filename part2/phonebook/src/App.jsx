865a2df096bab2ae94af1675a3a823ea59bffd69
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-02-03 20:14:21 +0700

    Render name and number

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -50,7 +50,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.id}>{person.name}</p>
+        <p key={person.id}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
