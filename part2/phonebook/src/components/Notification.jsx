c48b28d6016cba79db5ef8178ba4116edae8f237
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-03-07 15:52:14 +0700

    Add message and errorMessage logic in Notification

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,9 +1,13 @@
-const Notification = ({ message }) => {
-  if (message === null) {
-    return null;
+const Notification = ({ message, errorMessage }) => {
+  if (message) {
+    return <div className="success">{message}</div>;
   }
 
-  return <div className="success">{message}</div>;
+  if (errorMessage) {
+    return <div className="error">{errorMessage}</div>;
+  }
+
+  return null;
 };
 
 export default Notification;
