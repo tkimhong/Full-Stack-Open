b3ca84654b0e4854b36e0d869fe931e217eb72a7
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-03-24 23:37:19 +0700

    Add code to fetch countries data

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
   const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, []);
+
+  const countriesToShow = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(search.toLowerCase());
+  });
+
+  console.log("countries:", countries);
+  console.log("search:", search);
+  console.log("countriesToShow:", countriesToShow);
 
   return (
     <div>
