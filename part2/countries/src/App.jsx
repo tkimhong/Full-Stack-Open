d5dacc18b988b48e3aee97a3320244bc19ba686b
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-03-24 21:45:29 +0700

    Add basic search field

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,3 +1,16 @@
-const App = () => {};
+import { useState } from "react";
+
+const App = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div>
+      <div>
+        find countries{" "}
+        <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
