e74f3e200d0bdcee5cd0e011865ab7b1a688ef7e
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-03-26 12:52:04 +0700

    Show country details

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -32,6 +32,20 @@ const App = () => {
       <div>
         {countriesToShow.length > 10 ? (
           <p>Too many matches, specify another filter</p>
+        ) : countriesToShow.length === 1 ? (
+          <div>
+            <h1>{country.name.common}</h1>
+            <p>Capital {country.capital}</p>
+            <p>Area {country.area}</p>
+
+            <h3>Languages:</h3>
+            <ul>
+              {Object.values(country.languages).map((lang) => (
+                <li key={lang}>{lang}</li>
+              ))}
+            </ul>
+            <img src={country.flags.png} alt={country.name.common} />
+          </div>
         ) : (
           countriesToShow.map((country) => {
             return <div key={country.name.common}>{country.name.common}</div>;
