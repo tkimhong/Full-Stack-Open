b8ff45cc7478da98ba04a9c8858a0b117015b891
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-04-14 19:35:03 +0700

    Expand morgan middleware for POST requests

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("tiny"));
-
 app.use(express.json());
+
+// app.use(morgan("tiny"));
+morgan.token("body", (request, response) => {
+  return JSON.stringify(request.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 let persons = [
   {
