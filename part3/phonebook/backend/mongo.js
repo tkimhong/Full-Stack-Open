9e3c623b3294d4374922d6ce3b9edb24ce8a6ccb
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-06-11 17:53:02 +0700

    Add mongo.js creating person in Mongo

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+// TODO read from phonebook
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const phone = process.argv[4];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.6wcma9a.mongodb.net/phonebook?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// Inaccessible anyway (need to destructure this object)
+/* const person = new Person({
+  name: "Hong Daddy",
+  phone: "016126629",
+}); */
+
+person.save().then((result) => {
+  console.log(`added ${name} number ${phone} to phonebook`);
+  mongoose.connection.close();
+});
