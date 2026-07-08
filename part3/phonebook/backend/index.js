201216aadb7f1bbe91b88dfcf619bb0284df70a5
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-07-09 03:39:09 +0700

    Use error handling middleware in fetch

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -47,10 +47,16 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
