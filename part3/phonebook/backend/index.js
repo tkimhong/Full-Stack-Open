8282c02d90bd01032d2b4b087d6d04190692e211
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-07-09 03:33:51 +0700

    Use error handling middleware in delete

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -53,7 +53,7 @@ app.get("/api/persons/:id", (request, response) => {
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
